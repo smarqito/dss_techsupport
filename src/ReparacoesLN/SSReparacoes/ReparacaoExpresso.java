877c251a33bf2d21857bea888d6936739f71158f
José Malheiro <jafmalheiro@live.com.pt>
2021-12-28 21:42:43 +0000

    Started registaPassoReparacao repXpresso

--- src/ReparacoesLN/SSReparacoes/ReparacaoExpresso.java
+++ src/ReparacoesLN/SSReparacoes/ReparacaoExpresso.java
@@ -6,11 +6,38 @@ public class ReparacaoExpresso extends Reparacao {
 	private Integer tempoEstimado;
 	private String nome;
 
+	/**
+	 * Construtor para uma Reparação Expresso
+	 * 
+	 * @param precoFixo Preço da reparação
+	 * @param tempoEstimado Tempo estimado para a duração da reparação
+	 * @param nome Nome da reparação
+	 */
+	public ReparacaoExpresso(Double precoFixo, Integer tempoEstimado, String nome) {
+		this.precoFixo = precoFixo;
+		this.tempoEstimado = tempoEstimado;
+		this.nome = nome;
+	}
+
 	@Override
 	public CustoTotalReparacao getPrecoEfetivo() {
 		return null;
 	}
+	
+	/**
+	 * Método para realizar a reparação expresso na sua totalidade, dado a esta não possuir múltiplos passos
+	 * 
+	 */
+	@Override
+	public void registaPassoRealizado(Integer tempo, Double custo) {
+		
+		ReparacaoEstado novoEstado = ReparacaoEstado.reparado;
 
+		alteraEstado(novoEstado, "");
+	}
+
+	// Getters e Setters
+	
 	public Double getPrecoFixo() {
 		return precoFixo;
 	}
@@ -36,9 +63,36 @@ public class ReparacaoExpresso extends Reparacao {
 	}
 
 	@Override
-	public void registaPassoRealizado(Integer tempo, Double custo) {
-		// TODO Auto-generated method stub
-		
+	public String toString() {
+		return "ReparacaoExpresso [nome=" + nome + ", precoFixo=" + precoFixo + ", tempoEstimado=" + tempoEstimado
+				+ "]";
 	}
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ReparacaoExpresso other = (ReparacaoExpresso) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (precoFixo == null) {
+            if (other.precoFixo != null)
+                return false;
+        } else if (!precoFixo.equals(other.precoFixo))
+            return false;
+        if (tempoEstimado == null) {
+            if (other.tempoEstimado != null)
+                return false;
+        } else if (!tempoEstimado.equals(other.tempoEstimado))
+            return false;
+        return true;
+    }
 	
 }
