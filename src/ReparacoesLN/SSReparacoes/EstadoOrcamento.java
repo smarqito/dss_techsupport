090f4b4e347f5532d63b8a2b42b987047de40f9a
José Malheiro <jafmalheiro@live.com.pt>
2021-12-28 20:45:42 +0000

    Classe Orcamento done, exceto alteraEstado

--- src/ReparacoesLN/SSReparacoes/EstadoOrcamento.java
+++ src/ReparacoesLN/SSReparacoes/EstadoOrcamento.java
@@ -1,23 +1,52 @@
 package ReparacoesLN.SSReparacoes;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Comparator;
 
-public class EstadoOrcamento {
+public class EstadoOrcamento implements Serializable ,Comparator<EstadoOrcamento> {
 
 	private OrcamentoEstado estado;
 	private LocalDateTime data;
 
 	/**
+	 * Construtor para o Estado Orçamento que recebe o estado
 	 * 
-	 * @param s
+	 * @param s Enum com o estado do orçamento
 	 */
-	public EstadoOrcamento(OrcamentoEstado s) {
-		// TODO - implement EstadoOrcamento.EstadoOrcamento
-		throw new UnsupportedOperationException();
+	public EstadoOrcamento(OrcamentoEstado orcEstado) {
+		estado = orcEstado;
+		data = LocalDateTime.now();
 	}
+	
+	/**
+	 * Método compare para ordenar o TreeSet de Estados de Orçamento
+	 */
+	@Override
+	public int compare(EstadoOrcamento o1, EstadoOrcamento o2) {
+		
+		if(o2.data.isAfter(o1.data))
+			return 1;
+
+		else return 0;
+	}
+
+	// Getters e Setters
 
 	public OrcamentoEstado getEstado() {
 		return this.estado;
+	}
+	
+	public void setEstado(OrcamentoEstado estado) {
+		this.estado = estado;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
 	}
 
 }
--- src/ReparacoesLN/SSReparacoes/Orcamento.java
+++ src/ReparacoesLN/SSReparacoes/Orcamento.java
@@ -2,86 +2,123 @@ package ReparacoesLN.SSReparacoes;
 
 import ReparacoesLN.SSClientes.*;
 
+import java.io.Serializable;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 import ReparacoesLN.SSColaboradores.*;
 
-public class Orcamento {
+public class Orcamento implements Serializable {
 
+	private String id;
 	private Equipamento equipamento;
 	private ReparacaoProgramada reparacao;
-	private PlanoTrabalho plano;
-	private List<EstadoOrcamento> estados;
-	private List<Comunicacao> comunicacoes;
-	private String id;
-	private Date prazoReparacao;
+	private LocalDate prazoReparacao;
 	private Double preco;
 	private Double custoHora;
 	private String descrProb;
+	private PlanoTrabalho plano;
+	private TreeSet<EstadoOrcamento> estados;
+	private List<Comunicacao> comunicacoes;
 
 	/**
+	 * ID para auto incrementar
+	 */
+	private static int CURRENT_ID = 1;
+
+	/**
+	 * Retorna o ID atual e incrementa a variável para o próximo
+	 */
+	private static String getCurrentID() {
+		
+		int curr_id = CURRENT_ID;
+		up_ID();
+		
+		String id = String.valueOf(curr_id);
+
+		return id;
+	}
+
+	/**
+	 * Incrementa o variável static CURRENT_ID, para o próximo orçamento
+	 */
+	private static void up_ID() {
+		CURRENT_ID++;
+	}
+
+	/**
+	 * Construtor para criar um orçamento a partir de um equipamento e de uma descrição
 	 * 
-	 * @param e
-	 * @param descr
+	 * @param e Equipamento a reparar
+	 * @param descr Descrição do Orçamento
 	 */
 	public Orcamento(Equipamento e, String descr) {
-		// TODO - implement Orcamento.Orcamento
-		throw new UnsupportedOperationException();
+		this.id = getCurrentID();
+		this.reparacao = null;
+		this.equipamento = e;
+		this.prazoReparacao = null;
+		this.preco = 0.0;
+		this.custoHora = 0.0;
+		this.descrProb = descr;
+		this.plano = null;
+		this.estados = new TreeSet<EstadoOrcamento>();
+		this.estados.add(new EstadoOrcamento(OrcamentoEstado.porCalcular));
+		this.comunicacoes = new ArrayList<Comunicacao>();
 	}
-
-	public String getID() {
-		// TODO - implement Orcamento.getID
-		throw new UnsupportedOperationException();
-	}
-
-	public List<Material> getMaterial() {
-		// TODO - implement Orcamento.getMaterial
-		throw new UnsupportedOperationException();
-	}
-
-	public Date getPrazoRep() {
-		// TODO - implement Orcamento.getPrazoRep
-		throw new UnsupportedOperationException();
-	}
-
-	public PlanoTrabalho getPT() {
-		// TODO - implement Orcamento.getPT
-		throw new UnsupportedOperationException();
-	}
-
-	public FormaContacto getFormaContacto() {
-		// TODO - implement Orcamento.getFormaContacto
-		throw new UnsupportedOperationException();
-	}
-
+	
 	/**
+	 * Método que retorna o passo do Plano de Trabalhos associado ao Orçamento
 	 * 
-	 * @param nome
-	 * @param tempo
-	 * @param material
+	 * Utiliza o método da classe PlanoTrabalho com o mesmo nome
+	 * 
+	 * @param nomePasso nome do passo a procurar
+	 */
+	public PassoReparacao getPasso(String nomePasso) {
+		return plano.getPasso(nomePasso);
+	}
+	
+	/**
+	 * Método que adiciona um passo no Plano de Trabalhos do orçamento
+	 * 
+	 * @param nome Nome do Passo de Reparação a adicionar
+	 * @param tempo Tempo estimado para o passo
+	 * @param material Material usado no passo
 	 */
 	public void addPasso(String nome, Integer tempo, Material material) {
-		// TODO - implement Orcamento.addPasso
-		throw new UnsupportedOperationException();
+		plano.addPasso(nome, tempo, material);
 	}
-
+	
 	/**
+	 * Método que adiciona um subPasso no Plano de Trabalhos do Orçamento
 	 * 
-	 * @param nomeP
-	 * @param nomeSub
-	 * @param tempo
-	 * @param material
+	 * @param nomeP Nome do main passo onde vai ser feita a adição
+	 * @param nomeSub Nome do subpasso a adicionar
+	 * @param tempo Tempo estimado
+	 * @param material Material usado
 	 */
 	public void addSubPasso(String nomeP, String nomeSub, Integer tempo, Material material) {
-		// TODO - implement Orcamento.addSubPasso
-		throw new UnsupportedOperationException();
+		plano.addSubPasso(nomeP, nomeSub, tempo, material);
 	}
-
+	
+	/**
+	 * Verifica se Orçamento ainda está ativo
+	 * 
+	 * @return true se estive ativo, false caso contrário
+	 */
 	public Boolean estaAtivo() {
-		// TODO - implement Orcamento.estaAtivo
-		throw new UnsupportedOperationException();
-	}
+		
+		EstadoOrcamento estadoAtual = estados.first();
+		Boolean isAtivo = true;
 
+		if(estadoAtual.getEstado() == OrcamentoEstado.arquivado) {
+			isAtivo = false;
+		}
+
+		return isAtivo;
+	}
+	
 	/**
 	 * 
 	 * @param novoEstado
@@ -92,41 +129,146 @@ public class Orcamento {
 			this.estados.add(0, novoEstado);
 		}
 	}
-
-	public String generateResume() {
-		// TODO - implement Orcamento.generateResume
-		throw new UnsupportedOperationException();
-	}
-
+	
 	/**
+	 * Método que retorna numa String todas as informações de um Orçamento
+	 * @return
+	 */
+	public String generateResume() {
+		return "Orcamento [comunicacoes =" + comunicacoes + ", custoHora =" + custoHora + ", descrProb =" + descrProb
+				+ ", equipamento =" + equipamento + ", estado atual =" + estados.first() + ", id =" + id + ", plano =" + plano
+				+ ", prazoReparacao =" + prazoReparacao + ", preco =" + preco + ", reparacao =" + reparacao + "]";
+	}
+	
+	/**
+	 * Método que verifica se um passo exixste no plano de trabalhos do orçamento
 	 * 
-	 * @param nomePasso
+	 * @param nomePasso Nome do passo a procurar
 	 */
 	public Boolean existePasso(String nomePasso) {
-		// TODO - implement Orcamento.existePasso
-		throw new UnsupportedOperationException();
+		return plano.existePasso(nomePasso);
 	}
-
+	
 	/**
+	 * Método que adiciona uma comunicação efetuada com o cliente a lista de comunicações feitas
 	 * 
-	 * @param nomePasso
+	 * @param c Comunicação realizada
 	 */
-	public PassoReparacao getPasso(String nomePasso) {
-		// TODO - implement Orcamento.getPasso
-		throw new UnsupportedOperationException();
-	}
-
 	public void addComunicacao(Comunicacao c) {
 		this.comunicacoes.add(c);
 	}
-
+	
+	/**
+	 * Método que verifica se um orçamento enviado já passou o prazo estipulado para ser aceite
+	 * 
+	 * O prazo e de 30 dias.
+	 * 
+	 * @return true se passou de prazo, false caso contrário
+	 */
 	public Boolean passouPrazo() {
-		// TODO - implement Orcamento.passouPrazo
-		throw new UnsupportedOperationException();
+		
+		EstadoOrcamento estadoAtual = estados.first();
+		LocalDateTime tempoAtual = LocalDateTime.now();
+		Boolean passouPrazo = false;
+
+		if(estadoAtual.getEstado() == OrcamentoEstado.enviado) {
+			long days = ChronoUnit.DAYS.between(estadoAtual.getData(), tempoAtual);
+
+			if(days >= 30)
+				passouPrazo = true;
+		}
+
+		return passouPrazo;
 	}
 
+	//Getters e Setters
+
+	public String getID() {
+		return id;
+	}
+	
+	public Equipamento getEquipamento() {
+		return equipamento;
+	}
+	
+	public ReparacaoProgramada getReparacao() {
+		return reparacao;
+	}
+	
+	public void setReparacao(ReparacaoProgramada reparacao) {
+		this.reparacao = reparacao;
+	}
+	
+	public Double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	public Double getCustoHora() {
+		return custoHora;
+	}
+	
+	public void setCustoHora(Double custoHora) {
+		this.custoHora = custoHora;
+	}
+	
+	public String getDescrProb() {
+		return descrProb;
+	}
+	
+	public void setDescrProb(String descrProb) {
+		this.descrProb = descrProb;
+	}
+	
+	public TreeSet<EstadoOrcamento> getEstados() {
+		return estados;
+	}
+	
+	public List<Comunicacao> getComunicacoes() {
+		return comunicacoes;
+	}
+	
+	/**
+	 * Método que retorna todo o material necessário para efetuar o plano de trabalhos do orçamento
+	 * 
+	 * @return List de materiais
+	 */
+	public List<Material> getMaterial() {
+		//return plano.getMaterial();
+		return null;
+	}
+	
+	public LocalDate getPrazoRep() {
+		return prazoReparacao;
+	}
+
+	public void setPrazoRep(LocalDate prazoRep) {
+		prazoReparacao = prazoRep;
+	}
+	
+	public PlanoTrabalho getPT() {
+		return plano;
+	}
+	
 	public void setPT(List<PassoReparacao> passos) {
 		PlanoTrabalho pt = new PlanoTrabalho(passos);
 		this.plano = pt;
 	}
+
+	public void setPT(PlanoTrabalho plano) {
+		this.plano = plano;
+	}
+	
+	/**
+	 * Método para obter a forma de contacto do cliente cujo equipamento vai ser reparado
+	 * 
+	 * @return Forma de Contacto do cliente
+	 */
+	public FormaContacto getFormaContacto() {
+		return equipamento.getProprietario().getContacto();
+	}
+	
 }
