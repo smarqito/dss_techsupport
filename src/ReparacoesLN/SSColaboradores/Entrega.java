8fccaef91dc31243dd42e674fb71529713e2cc39
José Malheiro <jafmalheiro@live.com.pt>
2021-12-29 13:01:30 +0000

    Classe abstrata balcao done -> Rececao e Entrega done

--- src/ReparacoesLN/SSColaboradores/Entrega.java
+++ src/ReparacoesLN/SSColaboradores/Entrega.java
@@ -2,5 +2,18 @@ package ReparacoesLN.SSColaboradores;
 
 import java.io.Serializable;
 
+import ReparacoesLN.SSClientes.Equipamento;
+
 public class Entrega extends Balcao implements Serializable {
+
+    /**
+     * Construtor para a classe entrega
+     * Relativa à interação entre funcionário de balcão e cliente -> Entrega
+     * 
+     * @param equipamento Equipamento correspondente
+     * @param funcionario Funcionário que efetoou
+     */
+    public Entrega(Equipamento equipamento, FuncionarioBalcao funcionario) {
+        super(equipamento, funcionario);
+    }
 }
