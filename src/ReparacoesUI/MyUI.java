cf8ff6af1eb994fd35ab196ee6a283747e0e29c2
Miguel Fernandes <miguelffernandes13@gmail.com>
2021-12-29 10:12:13 +0000

    Menu principal e menus dos colaboradores, Menu

--- src/ReparacoesUI/MyUI.java
+++ src/ReparacoesUI/MyUI.java
@@ -1,14 +1,165 @@
 package ReparacoesUI;
 
+import Middleware.TecnicoJaTemAgendaException;
 import ReparacoesLN.*;
+
+import java.util.Scanner;
 
 public class MyUI {
 
 	private IReparacoesLN model;
 
+	private Scanner scin;
+
+	/**
+	 * Construtor.
+	 * <p>
+	 * Cria os menus e a camada de negócio.
+	 */
+	public MyUI() {
+		//inicial model
+		scin = new Scanner(System.in);
+	}
+
 	public void run() {
-		// TODO - implement MyUI.run
-		throw new UnsupportedOperationException();
+		System.out.println("Bem vindo ao Sistema DSS-TechSupport!");
+		this.menuPrincipal();
+		System.out.println("Até breve...");
+	}
+
+	private void menuPrincipal() {
+		Menu menu = new Menu(new String[]{
+				"Autenticar"
+		});
+
+		// mais pré-condições?
+
+		// Registar os handlers das transições
+		menu.setHandler(1, () -> autenticarColaborador());
+
+		// Executar o menu
+		menu.run();
+		System.out.println("Guardando...");
+		//this.model.save();
+
+	}
+
+	private void autenticarColaborador() {
+		System.out.println("Insira o seu número de identificação: ");
+		String id = scin.nextLine();
+		if (this.model.existeColaborador(id)) {
+			System.out.println("Acesso garantido");
+			switch (this.model.getColaborador().getClass.getSimpleName()){
+				case "Gestor":
+					this.menuGestor();
+					break;
+				case "Tecnico":
+					this.menuTecnico();
+					break;
+				case "FuncionarioBalcao":
+					this.menuFuncionarioBalcao();
+					break;
+			}
+		}
+	}
+
+	private void menuGestor(){
+		Menu menu = new Menu(new String[]{
+				"Registar Colaborador",
+				"Listar reparações detalhadas por técnico",
+				"Listar reparações resumidas por técnico",
+				"Listar fluxo de trabalho do funcionário de balcão"
+		});
+
+		// mais pré-condições?
+
+		// Registar os handlers das transições
+		menu.setHandler(1, this::registaColaborador);
+		menu.setHandler(2, this::listarDetalhadas);
+		menu.setHandler(3, this::listarResumidas);
+		menu.setHandler(4, this::listarFluxoFunc);
+
+		menu.run();
+	}
+
+
+	private void registaColaborador() {
+		try {
+			System.out.println("Nome do Colaborador: ");
+			String nome = scin.nextLine();
+			System.out.println("Função do Colaborador: ");
+			String tipo = scin.nextLine();
+			this.model.registaColaborador(nome, tipo);
+			System.out.println("Colaborador adicionado");
+		} catch (TecnicoJaTemAgendaException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void listarDetalhadas() {
+	}
+
+	private void listarFluxoFunc() {
+	}
+
+	private void listarResumidas() {
+	}
+
+	private void menuTecnico(){
+		Menu menu = new Menu(new String[]{
+				"Fazer orçamento",
+				"Realizar reparação"
+		});
+
+		// mais pré-condições?
+
+		// Registar os handlers das transições
+		menu.setHandler(1, this::fazerOrcamento);
+		menu.setHandler(2, this::realizarReparacao);
+
+		menu.run();
+	}
+
+	private void fazerOrcamento() {
+	}
+
+	private void realizarReparacao() {
+	}
+
+	private void menuFuncionarioBalcao(){
+		Menu menu = new Menu(new String[]{
+				"Registar cliente",
+				"Registar pedido de orçamento",
+				"Confirmar orçamento",
+				"Registar pedido de Reparação Expresso",
+				"Registar entrega do equipamento"
+		});
+
+		// mais pré-condições?
+
+		// Registar os handlers das transições
+		menu.setHandler(1, this::registaCliente);
+		menu.setHandler(2, this::pedidoOrcamento;
+		menu.setHandler(3, this::confirmarOrcamento);
+		menu.setHandler(4, this::pedidoRepXpresso);
+		menu.setHandler(5, this::registarEntregaEquip);
+
+		menu.run();
+	}
+
+	private void registaCliente() {
+	}
+
+	private void pedidoOrcamento() {
+	}
+
+	private void confirmarOrcamento() {
+	}
+
+	private void pedidoRepXpresso() {
+	}
+
+	private void registarEntregaEquip() {
 	}
 
 }
