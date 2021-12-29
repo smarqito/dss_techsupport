d04a65d2fbea4b8007a87df5cd69963147e6633a
Marco António Sousa <smarqito@gmail.com>
2021-12-29 14:03:37 +0000

    merge master

--- src/ReparacoesLN/SSColaboradores/Balcao.java
+++ src/ReparacoesLN/SSColaboradores/Balcao.java
@@ -3,12 +3,39 @@ package ReparacoesLN.SSColaboradores;
 import ReparacoesLN.SSClientes.*;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public abstract class Balcao implements Serializable{
 
 	private Equipamento equipamento;
 	private FuncionarioBalcao funcionario;
-	private Date data;
+	private LocalDateTime data;
+
+	public Balcao(Equipamento equipamento, FuncionarioBalcao funcionario) {
+		this.equipamento = equipamento;
+		this.funcionario = funcionario;
+		this.data = LocalDateTime.now();
+	}
+
+	public Equipamento getEquipamento() {
+		return equipamento;
+	}
+
+	public void setEquipamento(Equipamento equipamento) {
+		this.equipamento = equipamento;
+	}
+
+	public FuncionarioBalcao getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(FuncionarioBalcao funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
 
 }
--- src/ReparacoesLN/SSReparacoes/Reparacao.java
+++ src/ReparacoesLN/SSReparacoes/Reparacao.java
@@ -74,7 +74,7 @@ public abstract class Reparacao {
 
 	public void alteraEstado(ReparacaoEstado novoEstado, String msg) {
 		EstadoReparacao novo = new EstadoReparacao(novoEstado, msg);
-		this.estados.add(0, novo);
+		this.estados.add(novo);
 
 	}
 
